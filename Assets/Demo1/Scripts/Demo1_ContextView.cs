0cac03791ed3e0cca69e84ea435f8b4fff96bdde
FayeLee303 <1653090976@qq.com>
2017-08-02 00:25:48 +0800

    first commit

---
+++ Assets/Demo1/Scripts/Demo1_ContextView.cs
@@ -0,0 +1,17 @@
+﻿using strange.extensions.context.impl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//这是root，用来启动整个框架
+public class Demo1_ContextView : ContextView {
+
+    void Awake()
+    {
+        this.context = new Demo1_MVCSContext(this);//构造一个Context，this就是contextView
+        //context.Start();//启动整个框架
+        //启动框架后就会进行mapBindings绑定
+    }
+
+
+}
---
+++ Assets/Demo1/Scripts/Demo1_MVCSContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using strange.extensions.context.impl;
+using strange.extensions.context.api;
+
+//这是用来绑定的
+public class Demo1_MVCSContext : MVCSContext {
+
+    //在这里是给指定一个view，就是context要绑定的物体
+    public Demo1_MVCSContext(MonoBehaviour view) : base(view) { }
+
+    //进行绑定映射
+    protected override void mapBindings() {
+        //model
+
+        //service
+
+        //command
+
+        //mediator 这个其实就是view
+
+
+        //创建一个startCommand开始命令来做一些初始化命令，完成上面的绑定之后立即进行调用
+        //事件一般定义为枚举类型，Bind就是绑定，ContextEvent.START是内置的，把StartCommand和它绑定就会自动调用
+        commandBinder.Bind(ContextEvent.START).To<StartCommand>();
+    }
+
+    
+}
