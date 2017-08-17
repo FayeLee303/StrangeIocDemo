41fa9849fed5327bea7953d8817e6fd640537e35
FayeLee303 <1653090976@qq.com>
2017-08-18 01:03:31 +0800

    状态机管理类
    用枚举类型定义转换条件和所转换状态ID，FSMState类里放所有状态共用的功能，初始化一个字典，是根据当前条件转换到某个状态的字典，功能有添加/删除转换条件，判断某转换条件是否有对应状态，virtual进入当前状态和离开当前状态之前要做的事，之后可以重构这两个函数。FSMSystem类作为状态机管理类，做成单例模式，用字典保存当前状态机里有哪些状态，在构造函数里初始化字典，主要功能有向状态机里添加/删除状态，以及根据条件转换当前状态（需要先判断传过来的条件是否有对应的下一步状态）

---
+++ Assets/FrameWork/Scripts/FSM/FSMState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//有哪些状态转换的条件
+public enum Transition {
+    NUllTransition = 0
+}
+
+//状态标识符，一个状态只有一个ID且不可重复
+public enum StateID
+{
+    NUllStateID = 0
+}
+
+//每一个状态的共有功能
+public class FSMState  {
+    protected StateID stateID;  //protected是子类可以访问
+    public StateID GetStateID {
+        get { return stateID; }
+    }
+
+    //在某个条件下转换到某个ID的状态
+    protected Dictionary<Transition, StateID> mapDict = new Dictionary<Transition, StateID>();
+
+    //添加转换条件
+    public void AddTransition(Transition trans, StateID id) {
+        if (trans == Transition.NUllTransition || id == StateID.NUllStateID) {
+            Debug.LogError("Transition or StateID is null");
+            return;
+        }
+        if (mapDict.ContainsKey(trans)) {
+            Debug.LogError("StateId"+stateID + "已经有转换条件"+trans);
+            return;
+        }
+        mapDict.Add(trans,id);
+    }
+
+    //删除转换条件
+    public void DeleteTransition(Transition trans) {
+        if (mapDict.ContainsKey(trans)== false) {
+            Debug.LogWarning("转换条件"+trans +"不存在在字典里");
+            return;
+        }
+        mapDict.Remove(trans);
+    }
+
+    //如果发生了某个转换条件，看是否能转换成某个状态
+    public StateID GetOutPutState(Transition trans) {
+        if (mapDict.ContainsKey(trans))
+        {
+            return mapDict[trans];//这里是返回trans条件下需要转换的状态ID
+        }
+        else {
+            return StateID.NUllStateID;
+        }
+    }
+
+    //进入当前状态之前要做的事
+    public virtual void DoBeforeEntering() {
+        //这里可以初始化
+    }
+
+    //离开当前状态之前要做的事
+    public virtual void DoBeforeLeaving()
+    {
+        //这里可以清理
+    }
+}
---
+++ Assets/FrameWork/Scripts/FSM/FSMSystem.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//状态机管理类，有限状态机系统类
+public class FSMSystem  {
+    //用字典保存当前状态机里有哪些状态
+    private Dictionary<StateID,FSMState> statesDict;
+
+    private FSMState currentState;//状态机里的当前状态
+    //获取
+    public FSMState GetCurrentState {
+        get {
+            return currentState;
+        }
+    }
+
+    //构造函数
+    public FSMSystem() {
+        statesDict = new Dictionary<StateID, FSMState>();
+    }
+
+    //向状态机里添加状态
+    public void AddState(FSMState state) {
+        if (state == null) {
+            Debug.LogError("你想添加的状态"+state+"是空的");
+            return;
+        }
+        if (statesDict.ContainsKey(state.GetStateID)) {
+            //看字典里是否已经有想要添加的状态ID
+            Debug.LogError("你想添加的状态"+state.GetStateID+"已经存在");
+            return;
+        }
+        statesDict.Add(state.GetStateID,state);
+    }
+    //向状态机里删除状态
+    public void DeleteState(FSMState state) {
+        if (state == null)
+        {
+            Debug.LogError("你想添加的状态" + state + "是空的");
+            return;
+
+        }
+        if (statesDict.ContainsKey(state.GetStateID) == false)
+        {
+            //看字典里是否已经有想要添加的状态ID
+            Debug.LogError("你想添加的状态" + state.GetStateID + "已经被删除");
+            return;
+        }
+        statesDict.Remove(state.GetStateID);
+    }
+
+    //根据条件转换状态
+    public void PerformTrasition(Transition trans) {
+        if (trans == Transition.NUllTransition) {
+            Debug.LogError("空条件不能做转换条件");
+        }
+
+        //传过来一个条件，判断这个条件能不能转换成另一个状态
+        //GetOutPutState方法返回的是一个StateID值
+        StateID id = GetCurrentState.GetOutPutState(trans);
+        if (id == StateID.NUllStateID)
+        {
+            Debug.Log("给的条件无效，没有符合条件的转换");
+            return;
+        }
+        else {
+            FSMState state;
+            statesDict.TryGetValue(id, out state);//根据ID得到一个state
+            GetCurrentState.DoBeforeEntering();
+            currentState = state; //当前状态切换至新的状态
+            GetCurrentState.DoBeforeLeaving();
+        }
+    }
+}
